d93b3bc23b00f9c433e21bfa6077792ed860798e
FrankIsInDaHouse <156402956+FrankIsInDaHouse@users.noreply.github.com>
2024-01-30 15:43:53 -0500

    Update Ejercicio.py

--- Dia 4/Ejercicio.py
+++ Dia 4/Ejercicio.py
@@ -1,6 +1,22 @@
 t = int(input())
 
-for i in range(1,t+1,1):
-    print("Caso #", i)
+x = (input())
+nyk = [x]
+
+for i in range(2):
+    num=int(nyk[i])
+    nyk[i]=num
+
+y = (input())
+l = [y]
+
+for i2 in range(nyk[0]):
+    num2=int(l[i2])
+    l[i2]=num2
+
+def info (t):
 
 
+
+for c in range(1,t+1,1):
+    print("Caso #",c,":")
