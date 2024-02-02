d2499ca49fb5702c37a605f81f247267d604d25d
FrankIsInDaHouse <156402956+FrankIsInDaHouse@users.noreply.github.com>
2024-02-02 09:00:05 -0500

    Add files via upload

---
+++ Dia 7 modulos/Ejercicio_EsparzaFrank.py
@@ -0,0 +1,19 @@
+
+def conversion(x):
+        return [10] * (( x // 10 )) + [5] * ((x % 10) //5) + [1] * ((x % 10)%5)
+        
+    
+while True:
+    x = int(input())
+
+    if x < 1000 or x > 0:
+        print(conversion(x))
+        break
+    elif x < 0:
+        print("Valor no válido")
+    else:
+        x > 1000
+        print("Valor excedido")
+  
+
+## Desarrollado por FRANK SEBASTIÁN ESPARZA RIAÑO - 1097789710
